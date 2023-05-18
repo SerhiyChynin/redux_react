3c0ddbaed04eeb5076ee4da3bebc13e2cd0a5b6f
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-18 07:24:31 +0300

    add actions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+  console.log(cash);
+
+  const addCash = () => {
+    dispatch({type: "ADD_CASH", payload: 5})
+  }
+  
+  const getCash = () => {
+    dispatch({type: "GET_CASH", payload: 5})
+  
+}
+
+
   return (
     <div >
+      <div style={{display: "flex"}}>
+        <div style={{fontSize:"3rem"}}>{ cash}</div>
+        <button onClick={() => addCash()} >ADD_CASH</button>
+        <button onClick={() => getCash()}>GET_CASH</button>
+      </div>
 
     </div>
   );
