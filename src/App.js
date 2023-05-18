670cf48fd39952995a344bb96ba69c8441f4c9a7
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-18 13:16:48 +0300

    array redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction } from "./store/customerReducers";
 
 
 
@@ -7,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
     dispatch({type: "ADD_CASH", payload: cash})
@@ -15,17 +17,54 @@ function App() {
   const getCash = (cash) => {
     dispatch({type: "GET_CASH", payload: cash})
   
-}
+  }
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  const removeCustomer = (customer) => {
+    dispatch({ type: "REMOVE_CUSTOMERS", payload: customer.id })
+  }
+  const removeCustomerB = (customers) => {
+    dispatch({ type: "REMOVE_CUSTOMERS_BTN",  payload: customers})
+  }
 
 
   return (
-    <div >
-      <div style={{display: "flex"}}>
+    <div style={{textAlign:'center'}}>
+      <div style={{display: "flex", flexFlow: "column"}}>
         <div style={{fontSize:"3rem"}}>{ cash} $ </div>
-        <button onClick={() => addCash(Number(prompt()))} >ADD_CASH</button>
-        <button onClick={() => getCash(Number(prompt()))}>GET_CASH</button>
-      </div>
 
+        </div>
+        <div style={{display: "flex", justifyContent: 'center'}}>
+        <button style={{width:200, marginTop: 10}} onClick={() => addCash(Number(prompt()))} >ADD_CASH</button>
+        <button style={{width:200, marginTop: 10}} onClick={() => getCash(Number(prompt()))}>GET_CASH</button>
+      </div>
+      <hr />
+      <div style={{marginBottom: 10 }}>
+        <button style={{width:200, marginTop: 10}} onClick={() => addCustomer(prompt())}>ADD_CUSTOMER</button>
+        <button style={{width:200, marginTop: 10}} onClick={() => removeCustomerB(prompt())}>REMOVE_CUSTOMER</button>
+      </div>
+      {customers.length > 0 ?
+        <div style={{display:'flex', flexFlow: 'column'}}>
+          {customers.map(customer => 
+            <div onClick={() => removeCustomer(customer)}
+              style={{
+                width: '40%', fontSize: "2rem", marginTop: '5px',
+                border: '1px solid black', margin: 'auto', marginBottom: 5
+              }} key={customer.id}>
+              {customer.name}
+            </div>
+            )}
+        </div>
+        :
+        <div style={{fontSize:"2rem", marginTop: '10px'}}>
+          Клиенты отсутсвуют
+        </div>
+        }
     </div>
   );
 }
--- src/store/customerReducers.js
+++ src/store/customerReducers.js
@@ -4,13 +4,21 @@ const defaultState = {
     customers: []
 }
 
+const ADD_CUSTOMER = "ADD_CUSTOMER"; //типам создаем константы и подставляем,, так меньшая вероятность ошибиться, среда подскажет при ошибке
+const REMOVE_CUSTOMERS = "REMOVE_CUSTOMERS"; 
+
 export const customerReducer = (state = defaultState, action) => {    //reducer это обычная функция JS принимает state, action
     switch (action.type) {
-        case "ADD_CUSTOMER":
-            return {...state, cash: state.cash + action.payload } // изначально состояние в редаксе неизменно, мы каждый раз должны возвращать новый обьект 
-        case "GET_CUSTOMERS":    
-            return {...state, cash: state.cash - action.payload }
+        case ADD_CUSTOMER:
+            return { ...state, customers: [...state.customers, action.payload] } // изначально состояние в редаксе неизменно, мы каждый раз должны возвращать новый обьект 
+        case REMOVE_CUSTOMERS:    
+            return {...state, customers: state.customers.filter(customer => customer.id !== action.payload) }
+        case "REMOVE_CUSTOMERS_BTN":  
+            return {...state, customers: state.customers.filter(customer => customer.name !== action.payload) }
         default:
             return state;
     }
 }
+
+export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload})
+export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMERS, payload})
